02830663ee70fea275add892665156c8633e5f38
shalynishka225 <shalynishka225@gmail.com>
2021-01-27 00:09:27 +0200

    adding night theme

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,47 @@
+import {
+  ASYNC_INCREMENT,
+  CHANGE_THEME,
+  DECREMENT,
+  DISABLE_BUTTONS,
+  ENABLE_BUTTONS,
+  INCREMENT,
+} from "./types";
+import { combineReducers } from "redux";
+
+export function counterReducer(state = 0, action) {
+  if (action.type === INCREMENT) {
+    return state + 1;
+  }
+
+  if (action.type === DECREMENT) {
+    return state - 1;
+  }
+
+  if (action.type === ASYNC_INCREMENT) {
+    return state + 10;
+  }
+  return state;
+}
+
+const initialThemeState = {
+  value: "light",
+  disabled: false,
+};
+
+export function themeReducer(state = initialThemeState, action) {
+  switch (action.type) {
+    case CHANGE_THEME:
+      return { ...state, value: action.payload };
+    case ENABLE_BUTTONS:
+      return { ...state, disabled: false };
+    case DISABLE_BUTTONS:
+      return { ...state, disabled: true };
+    default:
+      return state;
+  }
+}
+
+export const rootReducer = combineReducers({
+  counter: counterReducer,
+  theme: themeReducer,
+});
